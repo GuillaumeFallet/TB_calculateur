62463091eebebcbed759bd76e8892c2d379615f1
GuillaumeFallet <chibre001@gmail.com>
2016-06-09 15:47:07 +0200

    2 charts - avec changement dynamiques centrales nucl

--- scripts/centrales_nucl.js
+++ scripts/centrales_nucl.js
@@ -1,7 +1,7 @@
-var centrales = { 
-    Beznau1: {name: "Beznau 1", constructYear: 1969, maxPower: 365, lifetime: 50, stopdate: 2019, percent: 11, production: 2.893 },
-	Beznau2: {name: "Beznau 2", constructYear: 1971, maxPower: 365, lifetime: 50, stopdate: 2021, percent: 11, production: 2.893 },
-	Gosgen: {name: "Gösgen", constructYear: 1979, maxPower: 1010, lifetime: 50, stopdate: 2029, percent: 30, production: 7.89 },
-	Leibstadt: {name: "Leibstadt", constructYear: 1984, maxPower: 1220, lifetime: 50, stopdate: 2034, percent: 37, production: 9.731 },
-	Muhleberg: {name: "Mühleberg", constructYear: 1972, maxPower: 373, lifetime: 47, stopdate: 2019, percent: 11, production: 2.893 }
+var centrales = {
+	Beznau1: {name: "Beznau 1", constructYear: 1969, maxPower: 365, lifetime: 50, stopdate: 2019, percent: 11, production: 2893 },
+	Beznau2: {name: "Beznau 2", constructYear: 1971, maxPower: 365, lifetime: 50, stopdate: 2021, percent: 11, production: 2893 },
+	Gosgen: {name: "Gösgen", constructYear: 1979, maxPower: 1010, lifetime: 50, stopdate: 2029, percent: 30, production: 7890 },
+	Leibstadt: {name: "Leibstadt", constructYear: 1984, maxPower: 1220, lifetime: 50, stopdate: 2034, percent: 37, production: 9731 },
+	Muhleberg: {name: "Mühleberg", constructYear: 1972, maxPower: 373, lifetime: 47, stopdate: 2019, percent: 11, production: 2893 }
 } ;
--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,22 @@
  * Created by Pydd on 06.06.2016.
  */
 
-
+var currentTime = new Date();
+var year = currentTime.getFullYear() ;
+var nucl_chart  ;
+var consoprod_chart ;
+var array_diff_past = [] ;
 
 $(function (){
+
   $.each(centrales,function(key,value)
     {
         var row = '<tr>' +
             '<td>'+value.name+'</td>' +
             '<td id="constructYear_'+key+'">'+value.constructYear+'</td>' +
             '<td>'+value.maxPower+'</td>' +
-            '<td><form><input class="input_lifetime" id="lifetime_'+key+'" size="1" type="number" min="0" max="100" value="'+value.lifetime+'"> années </form></td>' +
-            '<td><form><input class="input_stopdate" id="stopdate_'+key+'" size="1" type="number" min="2016" max="2100" value="'+value.stopdate+'"></form></td>' +
+            '<td><form><input class="input_lifetime" id="lifetime_'+key+'" size="1" type="number" min="'+(year-value.constructYear)+'" max="100" value="'+value.lifetime+'"> années </input></form></td>' +
+            '<td><form><input class="input_stopdate" id="stopdate_'+key+'" size="1" type="number" min="'+year+'" max="'+(value.constructYear+100)+'" value="'+value.stopdate+'"/></form></td>' +
             '<td>'+value.percent+'</td>' +
             '<td>'+value.production+'</td>' +
             '</tr>' ;
@@ -22,22 +27,79 @@ $(function (){
     $('.input_lifetime').change(function(event){
         var id = jQuery(this).attr("id");
         var splitid = id.split("_") ;
-       $('#stopdate_'+splitid[1]).attr("value",parseFloat($('#constructYear_'+splitid[1]).text()) + parseFloat($('#lifetime_'+splitid[1]).val())) ;
+        var value =  parseFloat($(this).val()) ;
+        var min = parseFloat($(this).attr("min")) ;
+        var max = parseFloat($(this).attr("max")) ;
+        var newValue = value ;
+        if( value < min) {
+            newValue = min ;
+        }
+        else if (value > max) {
+            newValue = max ;
+        }
+        $(this).prop("value",newValue) ;
+        $('#stopdate_'+splitid[1]).prop("value",parseFloat($('#constructYear_'+splitid[1]).text()) + newValue) ;
+        calculateNuclProd() ;
     });
 
     $('.input_stopdate').change(function(event){
         var id = jQuery(this).attr("id");
         var splitid = id.split("_") ;
-        $('#lifetime_'+splitid[1]).attr("value",parseFloat($('#stopdate_'+splitid[1]).val()) - parseFloat($('#constructYear_'+splitid[1]).text())) ;
+
+
+        var value =  parseFloat($(this).val()) ;
+        var min = parseFloat($(this).attr("min")) ;
+        var max = parseFloat($(this).attr("max")) ;
+        var newValue = value ;
+        if( value < min) {
+            newValue = min ;
+        }
+        else if (value > max) {
+            newValue = max ;
+        }
+
+        $(this).prop("value",newValue) ;
+        $('#lifetime_'+splitid[1]).prop("value",parseFloat(newValue - parseFloat($('#constructYear_'+splitid[1]).text()))) ;
+        calculateNuclProd() ;
 
     });
 }) ;
 
+function calculateNuclProd()
+{
+    var new_nucl_prod = Array.apply(null, Array(18)).map(Number.prototype.valueOf,0);
 
+    $('#id_nuclear_table > tbody').find('tr').each(function (i, el) {
+        var $tds = $(this).find('td') ;
+
+        for(var i = 0 ; i < 18 ; i ++)
+        {
+            if (i+year+1 < $tds.eq(4).find("input").val())
+            {
+              new_nucl_prod[i] += parseInt($tds.eq(6).text()) ;
+          //    console.log("année "+(i+year)+". La centrale de "+$tds.eq(0).text()+" produit encore") ;
+            }
+
+        }
+
+    });
+    var array_diff_futur = [];
+
+
+    for (var i = 0 ; i < conso_futur.length ; i ++)
+    {
+        // console.log((prod_hydro_past[i]+prod_nucl_past[i]+prod_therm_past[i])-conso_past[i]) ;
+        array_diff_futur.push(prod_hydro_futur[i]+new_nucl_prod[i]+prod_therm_futur[i]-conso_futur[i])
+    }
+
+    nucl_chart.series[2].setData(prod_nucl_past.concat(new_nucl_prod),true) ;
+    consoprod_chart.series[0].setData(array_diff_past.concat((array_diff_futur)),true) ;
+}
 
 $(function () {
-    $('#container').highcharts({
+        nucl_chart  = new Highcharts.Chart({
         chart: {
+            renderTo : container_nucl_chart,
             type: 'area'
         },
         title: {
@@ -51,7 +113,13 @@ $(function () {
             tickmarkPlacement: 'on',
             title: {
                 enabled: false
-            }
+            },
+            plotLines: [{
+                color: 'black', // Color value
+                value: year-1960, // Value of where the line will appear
+                width: 3, // Width of the line
+                zIndex: 10
+            }]
         },
         yAxis: {
             title: {
@@ -68,7 +136,6 @@ $(function () {
         },
         plotOptions: {
             area: {
-                stacking: 'normal',
                 lineColor: '#666666',
                 lineWidth: 1,
                 marker: {
@@ -78,18 +145,89 @@ $(function () {
             }
         },
         series: [{
+            marker: {
+                symbol: 'circle'
+            },
             name: 'Hydraulique',
-            data: prod_hydro
+            data: prod_hydro_past.concat(prod_hydro_futur),
+            symbol: 'circle',
+            stacking: 'normal'
+        },{
+            marker: {
+                symbol: 'circle'
+            },
+            name: 'Thermique',
+            data: prod_therm_past.concat(prod_therm_futur),
+            stacking: 'normal'
         }, {
+            marker: {
+                symbol: 'circle'
+            },
             name: 'Nucléaire',
             color : '#D7DF01',
-            data: prod_nucl
+            data: prod_nucl_past.concat(prod_nucl_futur),
+            stacking: 'normal'
         }, {
-            name: 'Thermique',
-            data: prod_therm
-        },/* {
+            marker: {
+                symbol: 'circle'
+            },
             name: 'Consommation',
-            data: conso
-        }*/]
+            data: conso_past.concat(conso_futur),
+            color: '#DF0101',
+            type: 'line'
+
+        }]
+
+    });
+
+
+    var array_diff_futur = [];
+    for (var i = 0 ; i < conso_past.length ; i ++)
+    {
+       // console.log((prod_hydro_past[i]+prod_nucl_past[i]+prod_therm_past[i])-conso_past[i]) ;
+        array_diff_past.push(prod_hydro_past[i]+prod_nucl_past[i]+prod_therm_past[i]-conso_past[i])
+    }
+    for (var i = 0 ; i < conso_futur.length ; i ++)
+    {
+        // console.log((prod_hydro_past[i]+prod_nucl_past[i]+prod_therm_past[i])-conso_past[i]) ;
+        array_diff_futur.push(prod_hydro_futur[i]+prod_nucl_futur[i]+prod_therm_futur[i]-conso_futur[i])
+    }
+
+    consoprod_chart  = new Highcharts.Chart({
+        chart: {
+            renderTo : container_cons_prod_chart
+        },
+        title: {
+            text: 'Différence entre consommation et production'
+        },
+        subtitle: {
+            text: 'Source: Statistique suisse de l\x27électricité 2014'
+        },
+        xAxis: {
+            categories: years,
+            text: 'Années',
+            plotLines: [{
+                color: 'black', // Color value
+                value: year-1960, // Value of where the line will appear
+                width: 3, // Width of the line
+                zIndex: 10
+            }]
+        },
+        yAxis: {
+            title: {
+                text: 'Difference (GWh)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 2,
+                color: 'black'
+            }]
+
+
+        },
+        series: [{
+            name: 'Différence entre consommation et production',
+            data: array_diff_past.concat(array_diff_futur)
+        }]
     });
 });
