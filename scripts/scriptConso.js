6f2023ef5b4dcaed85b3b1939829d3ada11239b9
GuillaumeFallet <guillaume.fallet@students.hevs.ch>
2016-07-04 17:12:18 +0200

    ajout fin sprint 2

--- scripts/scriptConso.js
+++ scripts/scriptConso.js
@@ -7,8 +7,8 @@ $(function (){
     $('#evolution_conso').change(function(event) {
         var conso_today = globalArray['conso'][globalArray['years'].indexOf(year-2)];
         var rate = $('#evolution_conso').prop("value");
-        var last_conso_futur =  changeFuturConsoChart(rate) ;
-        var goal = (last_conso_futur-conso_today) / 1000 ;
+        changeFuturConsoChart(rate) ;
+        var goal = (globalArray['conso'][globalArray['years'].indexOf(2050)]-conso_today) / 1000 ;
         goal = Math.round(goal) ;
         $('#evolution_conso_goal').prop("value",goal) ;
 
@@ -16,15 +16,10 @@ $(function (){
 
     $('#evolution_conso_goal').change(function(event){
         var conso_today = globalArray['conso'][globalArray['years'].indexOf(year-2)];
-        var goal =  parseInt($('#evolution_conso_goal').prop("value"))*1000 + parseInt(conso_today) ;
-      //  console.log("Le goal est "+goal) ;
-        var percenttotal = goal/conso_today*100-100 ;
-     //   console.log("le % total est "+percenttotal) ;
-        var rate = percenttotal/ (2050-year-2) ;
-      //  console.log("le rate par année est de "+rate);
-        rate = Math.round(rate*10)/10 ;
+        var goal =  parseInt($('#evolution_conso_goal').prop("value"))*1000 ;
+
+        var rate = findRate(conso_today,goal) ;
         $('#evolution_conso').prop("value",rate) ;
-        
         changeFuturConsoChart(rate);
 
     }) ;
@@ -33,19 +28,9 @@ $(function (){
 
 function changeFuturConsoChart(rate)
 {
-
-    var conso_today = globalArray['conso'][globalArray['years'].indexOf(year-2)];
-
-    var j = 1 ;
-    for(var i = year-1 ; i <= 2050 ; i ++)
-    {
-        globalArray['conso'][globalArray['years'].indexOf(i)] =Math.round(conso_today/100*(100+rate*j)) ;
-        j ++ ;
-    }
-
+    applyRate(globalArray['conso'],rate) ;
     updateProdChart() ; 
-    updateConsProdChart() ; 
-    return  globalArray['conso'][globalArray['conso'].length-1] ;
+    updateConsProdChart() ;
 }
 
 function updateConsProdChart()
--- scripts/scriptProd.js
+++ scripts/scriptProd.js
@@ -16,16 +16,11 @@ function calculateProd()
     var hydro_fil_rate = parseFloat($('#evolution_hydrofil').prop("value")) ;
     var solar_rate = parseFloat($('#evolution_solar').prop("value")) ;
     var eol_rate = parseFloat($('#evolution_eolien').prop("value")) ;
-    var therm_rate = parseFloat($('#evolution_geothe').prop("value")) ;
 
-    for(var i = year-1 ; i <= 2050 ; i ++)
-    {
-        globalArray['prod_hydro_acc'][globalArray['years'].indexOf(i)] =Math.round(globalArray['prod_hydro_acc'][globalArray['years'].indexOf(i-1)]*(1+hydro_acc_rate/100)) ;
-        globalArray['prod_hydro_fil'][globalArray['years'].indexOf(i)] =Math.round(globalArray['prod_hydro_fil'][globalArray['years'].indexOf(i-1)]*(1+hydro_fil_rate/100))
-        globalArray['prod_solar'][globalArray['years'].indexOf(i)] =Math.round(globalArray['prod_solar'][globalArray['years'].indexOf(i-1)]*(1+solar_rate/100))
-        globalArray['prod_eol'][globalArray['years'].indexOf(i)] =Math.round(globalArray['prod_eol'][globalArray['years'].indexOf(i-1)]*(1+eol_rate/100))
-        globalArray['prod_gaz_centr'][globalArray['years'].indexOf(i)] =Math.round(globalArray['prod_gaz_centr'][globalArray['years'].indexOf(i-1)]*(1+therm_rate/100))
-    }
+    applyRate(globalArray['prod_hydro_acc'],hydro_acc_rate) ;
+    applyRate(globalArray['prod_hydro_fil'],hydro_fil_rate) ;
+    applyRate(globalArray['prod_solar'],solar_rate) ;
+    applyRate(globalArray['prod_eol'],eol_rate) ;
     
     updateProdChart() ;
     updateConsProdChart() ;
@@ -36,7 +31,7 @@ function updateProdChart() {
     prod_chart.series[0].update(globalArray['prod_solar'],true) ;
     prod_chart.series[1].update(globalArray['prod_eol'],true) ;
     prod_chart.series[2].update(globalArray['prod_hydro_acc'],true) ;
-    prod_chart.series[3].update(globalArray['prod_gaz_centr'],true) ;
+    // prod_chart.series[3].update(globalArray['prod_gaz_centr'],true) ;
     prod_chart.series[4].update(globalArray['prod_nucl'],true) ;
     prod_chart.series[5].update(globalArray['conso'],true) ;
 }
