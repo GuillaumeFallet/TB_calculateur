a6047c7e74362065dbbc67fc3f10967c59e512aa
GuillaumeFallet <chibre001@gmail.com>
2016-07-14 18:05:59 +0200

    fin doc

--- documents/scenarii.js
+++ documents/scenarii.js
@@ -7,9 +7,8 @@
 // data file containing all the scenarii 
 var prodScenariiArray = [
     {id : 0, hydro : 2.5, solar : 24, eol : 3.5, geoth : 0, gaz : 0, text : 'Scénario de l\'AES - Voie d\'escalade'},
-    {id : 1, hydro : 2, solar : 19, eol : 3, geoth : 0, gaz : 4, text : 'Scénario de l\'AES - Sentier alpin'},
-    {id : 2, hydro : 0, solar : 20, eol : 2, geoth : 0, gaz : 0, text : 'Scénario de l\'OFEN - Stratégie énergétique 2050'},
-    {id : 3, hydro : 2.5, solar : 3, eol : 4, geoth : 2.2, gaz : 0, text : 'Scénario de Greenpeace'}
+    {id : 1, hydro : 0, solar : 20, eol : 2, geoth : 0, gaz : 0, text : 'Scénario de l\'OFEN - Stratégie énergétique 2050'},
+    {id : 2, hydro : 2.5, solar : 3, eol : 4, geoth : 2.2, gaz : 0, text : 'Scénario de Greenpeace'}
 
 ] ;
 
